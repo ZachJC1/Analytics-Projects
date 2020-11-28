e9fffc16273a04ec0e9975c785e053374a0d81fa
ZachJC1 <72150668+ZachJC1@users.noreply.github.com>
2020-11-28 13:29:18 -0800

    Add files via upload

---
+++ DatasetProcedures.vb
@@ -0,0 +1,213 @@
+﻿Module DatasetProcedures
+
+    Public Function GetMaxMargin() As Double
+        Dim Value As String
+        For Each myRow As DataRow In myDataSet.Tables("EnvironmentVariableTable").Rows
+            If myRow("Name").Trim() = "MaxMargins" Then
+                Value = myRow("Value")
+                Return Double.Parse(Value)
+            End If
+        Next
+        MessageBox.Show("Shoot, I couldn't find Max Margins. Returned 0.")
+        Return 0
+    End Function
+
+    Public Function GetStartDate() As Date
+        Dim value As String
+        For Each myRow As DataRow In myDataSet.Tables("EnvironmentVariableTable").Rows
+            If myRow("Name").Trim() = "StartDate" Then
+                value = myRow("Value")
+                Return Date.Parse(value)
+            End If
+        Next
+        MessageBox.Show("Holy BatScreen! Could not find 'StartDate'. Returned 1/1/1.")
+        Return "1/1/1"
+    End Function
+
+
+    Public Function GetRiskFreeRate() As Double
+        Dim Value As String
+        For Each myRow As DataRow In myDataSet.Tables("EnvironmentVariableTable").Rows
+            If myRow("Name").Trim() = "RiskFreeRate" Then
+                Value = myRow("Value")
+                Return Double.Parse(Value)
+            End If
+        Next
+        MessageBox.Show("Shoot, I couldn't find RiskFreeRate. Returned 0.")
+        Return 0
+
+    End Function
+
+    Public Function GetInitialCAccount() As Double
+        Dim Value As String
+        For Each myRow As DataRow In myDataSet.Tables("EnvironmentVariableTable").Rows
+            If myRow("Name").Trim() = "CAccount" Then
+                Value = myRow("Value")
+                Return Double.Parse(Value)
+            End If
+        Next
+        MessageBox.Show("Shoot, I couldn't find CAccount. Returned 0.")
+        Return 0
+    End Function
+
+    Public Function GetCAccount() As Double
+        Dim Value As String
+        For Each myrow As DataRow In myDataSet.Tables("AcquiredPositionsTable").Rows
+            If myrow("symbol").trim() = "CAccount" Then
+                Value = myrow("units")
+                Return Double.Parse(Value)
+            End If
+
+        Next
+        MessageBox.Show("Zoinks! I couldn't find 'CAccount'. Returned 0.")
+        Return 0
+    End Function
+
+
+    Public Function IsAStock(symbol As String) As Boolean
+        symbol = symbol.Trim()
+        For Each myRow As DataRow In myDataSet.Tables("TickerTable").Rows
+            If myRow("Ticker").trim() = symbol Then
+                Return True
+            End If
+        Next
+        Return False
+    End Function
+
+    Public Function GetAsk(symbol As String, targetDate As Date) As Double
+        symbol = symbol.Trim()
+        If (targetDate.DayOfWeek = DayOfWeek.Saturday) Then
+            targetDate = targetDate.AddDays(-1)
+        End If
+        If (targetDate.DayOfWeek = DayOfWeek.Sunday) Then
+            targetDate = targetDate.AddDays(-2)
+        End If
+
+        DownloadPricesForOneDay(targetDate)
+
+        If IsAStock(symbol) Then
+            For Each myRow As DataRow In myDataSet.Tables("StockMarketOneDayTable").Rows
+                If myRow("Ticker").trim() = symbol Then
+                    Return myRow("Ask")
+                End If
+            Next
+        Else ' is an option
+            For Each myRow As DataRow In myDataSet.Tables("OptionMarketOneDayTable").Rows
+                If myRow("Symbol").trim() = symbol Then
+                    Return myRow("Ask")
+                End If
+            Next
+        End If
+        MessageBox.Show("Darn! Could not find the ask for " + symbol + ". Returned 0.")
+        Return 0
+    End Function
+
+    Public Function GetBid(symbol As String, targetDate As Date) As Double
+        symbol = symbol.Trim()
+        If (targetDate.DayOfWeek = DayOfWeek.Saturday) Then
+            targetDate = targetDate.AddDays(-1)
+        End If
+        If (targetDate.DayOfWeek = DayOfWeek.Sunday) Then
+            targetDate = targetDate.AddDays(-2)
+        End If
+
+        DownloadPricesForOneDay(targetDate)
+
+        If IsAStock(symbol) Then
+            For Each myRow As DataRow In myDataSet.Tables("StockMarketOneDayTable").Rows
+                If myRow("Ticker").trim() = symbol Then
+                    Return myRow("Bid")
+                End If
+            Next
+        Else ' is an option
+            For Each myRow As DataRow In myDataSet.Tables("OptionMarketOneDayTable").Rows
+                If myRow("Symbol").trim() = symbol Then
+                    Return myRow("Bid")
+                End If
+            Next
+        End If
+        MessageBox.Show("Darn! Could not find the bid for " + symbol + ". Returned 0.")
+        Return 0
+    End Function
+
+    Public Function GetDividend(ticker As String, targetDate As Date) As Double
+        If IsAStock(ticker) Then
+            If (targetDate.DayOfWeek = DayOfWeek.Saturday) Then
+                targetDate = targetDate.AddDays(-1)
+            End If
+            If (targetDate.DayOfWeek = DayOfWeek.Sunday) Then
+                targetDate = targetDate.AddDays(-2)
+            End If
+
+            DownloadPricesForOneDay(targetDate)
+
+            For Each myRow As DataRow In myDataSet.Tables("StockMarketOneDayTable").Rows
+                If myRow("Ticker").trim() = ticker Then
+                    Return Double.Parse(myRow("Dividend"))
+                End If
+            Next
+        End If
+        MessageBox.Show("Holy Batshoelace! I could not find the dividend for " + ticker + ". Returned 0.")
+        Return 0
+    End Function
+
+    Public Function GetStrike(symbol As String)
+        For Each myRow As DataRow In myDataSet.Tables("OptionMarketOneDayTable").Rows
+            If myRow("Symbol").trim() = symbol Then
+                Return Double.Parse(myRow("Strike"))
+            End If
+        Next
+        MessageBox.Show("Holy Batshoelace! I could not find the strike for " + symbol + ". Returned 0.")
+        Return 0
+    End Function
+
+    Public Function GetTrCostCoefficient(secType As String, trType As String) As Double
+        For Each myRow As DataRow In myDataSet.Tables("TransactionCostTable").Rows
+            If myRow("SecurityType").trim() = secType And myRow("TransactionType").trim() = trType Then
+                Return Double.Parse(myRow("CostCoeff"))
+            End If
+        Next
+        MessageBox.Show("Holy Batshoelace! I could not find the transaction cost. Returned 0.")
+        Return 0
+    End Function
+
+    Public Function GetCurrentPositionInAP(symbol) As Double
+        For Each myRow As DataRow In myDataSet.Tables("AcquiredPositionsTable").Rows
+            If myRow("symbol").ToString().Trim() = symbol Then
+                Return Double.Parse(myRow("Units"))
+            End If
+        Next
+        Return 0
+    End Function
+
+    Public Function GetUnderlier(symbol As String) As String
+        symbol = symbol.Trim()
+        For Each myRow As DataRow In myDataSet.Tables("OptionMarketOneDayTable").Rows
+            If myRow("Symbol").trim() = symbol Then
+                Return myRow("Underlier").Trim()
+            End If
+        Next
+        MessageBox.Show("Holy BatCucumber! I could not find the underlier for " + symbol + ". Returned ???")
+        Return "???"
+    End Function
+
+    Public Function IsACall(symbol As String) As Boolean
+        symbol = symbol.Trim()
+        For Each myRow As DataRow In myDataSet.Tables("OptionMarketOneDayTable").Rows
+            If myRow("Symbol").trim() = symbol And myRow("Type").trim() = "Call" Then
+                Return True
+            End If
+        Next
+        Return False
+    End Function
+
+    Public Function IsAPut(symbol As String) As Boolean
+        symbol = symbol.Trim()
+        For Each myRow As DataRow In myDataSet.Tables("OptionMarketOneDayTable").Rows
+            If myRow("Symbol").trim() = symbol And myRow("Type").trim() = "Put" Then
+                Return True
+            End If
+        Next
+        Return False
+    End Function
+End Module
---
+++ PortfolioManagement.vb
@@ -0,0 +1,151 @@
+﻿Module PortfolioManagement
+
+    Public Function CalcMTM(symbol As String, targetDate As Date) As Double
+        If symbol = "CAccount" Then
+            Return 1
+        Else
+            Return (GetAsk(symbol, targetDate) + GetBid(symbol, targetDate)) / 2
+        End If
+    End Function
+
+    Public Function CalcAPValue(targetDate As Date) As Double
+        Dim cumulativeAPValue As Double = 0
+        Dim symbol As String
+        Dim units As Double
+        Dim posValue As Double
+
+        For Each myRow As DataRow In myDataSet.Tables("AcquiredPositionsTable").Rows
+            symbol = myRow("Symbol").ToString().Trim
+            units = myRow("Units")
+            If symbol <> "CAccount" Then
+                posValue = units * CalcMTM(symbol, targetDate)
+                cumulativeAPValue = cumulativeAPValue + posValue
+                myRow("Value") = posValue
+            End If
+        Next
+        Return cumulativeAPValue
+    End Function
+
+    Public Function CalcIPValue(targetDate As Date) As Double
+        Dim cumulativeIPValue As Double = 0
+        Dim symbol As String
+        Dim units As Double
+        Dim posValue As Double
+
+        For Each myRow As DataRow In myDataSet.Tables("InitialPositionsTable").Rows
+            symbol = myRow("Symbol").ToString().Trim
+            units = myRow("Units")
+            If symbol <> "CAccount" Then
+                posValue = units * CalcMTM(symbol, targetDate)
+                cumulativeIPValue = cumulativeIPValue + posValue
+                myRow("Value") = posValue
+            End If
+        Next
+        Return cumulativeIPValue
+    End Function
+
+    Public Function CalcMargin(targetDate As Date) As Double
+        Return CalcAPMargin(targetDate) + CalcIPMargin(targetDate)
+    End Function
+
+    Public Function CalcAPMargin(targetDate As Date) As Double
+        Dim cumulativeAPMValue As Double = 0
+        Dim symbol As String
+        Dim units As Double
+        Dim posValue As Double
+        For Each myRow As DataRow In myDataSet.Tables("AcquiredPositionsTable").Rows
+            symbol = myRow("Symbol").ToString().Trim
+            units = myRow("Units")
+            If symbol <> "CAccount" And units < 0 Then
+                posValue = units * CalcMTM(symbol, targetDate)
+                cumulativeAPMValue = cumulativeAPMValue + posValue
+            End If
+        Next
+        Return cumulativeAPMValue
+    End Function
+
+    Public Function CalcIPMargin(targetDate As Date) As Double
+        Dim cumulativeIPMValue As Double = 0
+        Dim symbol As String
+        Dim units As Double
+        Dim posValue As Double
+        For Each myRow As DataRow In myDataSet.Tables("InitialPositionsTable").Rows
+            symbol = myRow("Symbol").ToString().Trim
+            units = myRow("Units")
+            If symbol <> "CAccount" And units < 0 Then
+                posValue = units * CalcMTM(symbol, targetDate)
+                cumulativeIPMValue = cumulativeIPMValue + posValue
+            End If
+        Next
+        Return cumulativeIPMValue
+    End Function
+
+    Public Function CalcTPVatStart() As Double
+        Return CalcIPValue(startDate) + initialCAccount
+    End Function
+
+    Public Function CalcTaTPV(targetDate As Date) As Double
+        Dim ts As TimeSpan = targetDate.Date - startDate.Date
+        Dim t As Double = ts.Days / 365.25
+        Return TPVatStart * Math.Exp(riskFreeRate * t)
+    End Function
+
+    Public Function IsInIP(x As String) As Boolean
+        x = x.Trim()
+        For Each myRow As DataRow In myDataSet.Tables("InitialPositionsTable").Rows
+            If myRow("Symbol").trim() = x Then
+                Return True
+            End If
+        Next
+        Return False
+    End Function
+
+    Public Function CalcTPV(currentDate) As Double
+        Return IPValue + APValue + CAccount + CalcInterestSLT(currentDate)
+    End Function
+
+    Public Function CalcInterestSLT(toThisDay As Date) As Double
+        Dim ts As TimeSpan = toThisDay.Date - lastTransactionDate.Date
+        Dim t As Double = ts.Days / 365.25
+        Return CAccount * (Math.Exp(riskFreeRate * t) - 1)
+    End Function
+
+    Public Function CalcTE() As Double
+        If TPV >= TaTPV Then
+            Return (TPV - TaTPV) * 0.25
+        Else
+            Return TaTPV - TPV
+        End If
+    End Function
+
+    Public Function UpdateSumTE(targetDate As Date) As Double
+        If targetDate.DayOfWeek = DayOfWeek.Sunday And targetDate.Date > lastTEUpdate.Date Then
+            lastTEUpdate = targetDate
+            Return TE
+        Else
+            Return 0
+        End If
+    End Function
+
+    Public Sub Execute(t As Transaction)
+        Dim mySQL As String
+        mySQL = String.Format("INSERT INTO TransactionQueue (Date, TeamID, Symbol, Type, Qty, Price, Cost, TotValue, " +
+                              "InterestSinceLastTransaction, CashPositionAfterTransaction, TotMargin) VALUES " + "('{0}', {1}, '{2}', '{3}', {4}, {5}, {6}, {7}, {8}, {9}, {10})",
+                              currentDate.ToShortDateString(),
+                              TeamID,
+                              t.symbol,
+                              t.type,
+                              t.qty,
+                              t.price,
+                              t.tcost,
+                              t.totValue,
+                              t.interestSLT,
+                              t.CAccountAT,
+                              t.marginAT)
+        RunNonQuery(mySQL)
+        lastTransactionDate = currentDate
+        CAccount = t.CAccountAT
+        margin = t.marginAT
+        'UpdatePosition(t.type, t.symbol, t.qty)
+    End Sub
+End Module
